f67b77f5f8bdd787ccee5a9b7db7b87ead294071
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2016-03-23 22:05:56 +0100

    import documentation

---
+++ implementors/hyper/server/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['edge'] = ["impl&lt;T: 'static + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a>&gt; <a class='trait' href='https://hyperium.github.io/hyper/hyper/server/trait.Handler.html' title='hyper::server::Handler'>Handler</a> for <a class='struct' href='edge/struct.Container.html' title='edge::Container'>Container</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/sys/ext/io/trait.FromRawSocket.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['edge'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/ext/io/trait.FromRawSocket.html' title='std::sys::ext::io::FromRawSocket'>FromRawSocket</a> for <a class='struct' href='https://hyperium.github.io/hyper/hyper/net/struct.HttpListener.html' title='hyper::net::HttpListener'>HttpListener</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/ext/io/trait.FromRawSocket.html' title='std::sys::ext::io::FromRawSocket'>FromRawSocket</a> for <a class='struct' href='https://hyperium.github.io/hyper/hyper/net/struct.HttpStream.html' title='hyper::net::HttpStream'>HttpStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
